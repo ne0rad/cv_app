91aeb781364c9004c04ef05ddc7a06d79dc3633c
Radminas Usackas <usackasr@gmail.com>
2021-11-04 12:42:10 +0000

    Updated Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,17 @@
-function Menu({editMode, setEditMode}) {
+function Menu({ editMode, setEditMode }) {
     return (
         <div className="menu">
-        <div className="menu-item">
-          <div className="text inline">Edit Mode:&nbsp;</div>
-          <label className="switch">
-            <input onClick={() => setEditMode(!editMode)} type="checkbox" />
-            <span className="slider round"></span>
-          </label>
+            <div className="menu-item">
+                <div className="text inline">Edit Mode:&nbsp;</div>
+                <label className="switch">
+                    <input onClick={() => setEditMode(!editMode)} type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+            </div>
+            <div className="menu-item">
+                <button className="print-btn">Download as PDF</button>
+            </div>
         </div>
-        <div className="menu-item">
-          <button className="print-btn">Download as PDF</button>
-        </div>
-      </div>
     )
 }
 

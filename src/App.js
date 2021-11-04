ddd9da65685172c0f023f94cba3d6dd1a1dcae9c
Radminas Usackas <usackasr@gmail.com>
2021-11-04 12:41:24 +0000

    Added menu component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Page from './components/Page';
+import Menu from './components/Menu';
 import './styles/App.css';
 import './styles/switch.css';
 import { useState } from 'react';
@@ -7,19 +8,13 @@ function App() {
   const [editMode, setEditMode] = useState(false);
   return (
     <div id="content">
-      <div className="menu">
-        <div className="menu-item">
-          <div className="text inline">Edit Mode:&nbsp;</div>
-          <label className="switch">
-            <input onClick={() => setEditMode(!editMode)} type="checkbox" />
-            <span className="slider round"></span>
-          </label>
-        </div>
-        <div className="menu-item">
-          <button className="print-btn">Download as PDF</button>
-        </div>
-      </div>
-      <Page editMode={editMode} />
+      <Menu
+        editMode={editMode}
+        setEditMode={setEditMode}
+      />
+      <Page
+        editMode={editMode}
+      />
     </div>
   );
 }
